68ef83576966326cae443c2f405802258afe6fc1
Rafael Araujo <rafael.araujo@comtex.com.br>
2021-08-20 16:23:22 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2e7f5687a942b09f2cb5360409568daf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de403fda-f00a-4cac-b6ad-1fe611b65e20'
+};
